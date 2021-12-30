e42b886e10d758e5926ef3e429b3aae4be319f28
unknown <guysela567@gmail.com>
2021-12-30 08:30:52 +0200

    Fixed import issue with collision hitbox and vector

--- utils/geometry/vector.py
+++ utils/geometry/vector.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
 from utils.constants import Constants
-from utils.geometry.collision import Hitbox
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from utils.geometry.collision import Hitbox
 
 import math
 
