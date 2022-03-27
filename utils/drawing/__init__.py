0351fd914fd35a6bc8ac319ae34c572cd450e22a
guysela567 <guysela567@gmail.com>
2022-03-27 13:15:10 +0300

    documented the drawing package

--- utils/drawing/__init__.py
+++ utils/drawing/__init__.py
@@ -1,3 +1,9 @@
+'''This package is my wrapper on the pygame library. 
+I designed this package to resemble the processing.org IDE which is originaly based on JAVA.
+It allows to start graphical projects quickly and easily
+and contains various functions for drawing and handling events.
+'''
+
 from os import environ
 
 environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1' # Get rid of pygame support message
